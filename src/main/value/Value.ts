4b835060c90c0c3a50bf308c6f3f86605a6f2877
Gonçalo Marques <goncalodinismarques@gmail.com>
2017-04-25 22:58:41 +0100

    Value now checks for predicate fulfilment

--- src/main/value/Value.ts
+++ src/main/value/Value.ts
@@ -4,6 +4,15 @@ export abstract class Value<T> implements Iterable<T> {
         return value;
     }
 
+    exists(predicate: (T) => boolean): boolean {
+        for (let v of this) {
+            if(predicate(v)){
+                return true;
+            }
+        }
+        return false;
+    }
+
     abstract get(): T;
 
     abstract [Symbol.iterator](): Iterator<T>;
--- src/test/value/Value.ts
+++ src/test/value/Value.ts
@@ -10,11 +10,34 @@ describe('Value', () => {
     expect(valueAny.get()).to.equal(valueStr.get());
   });
 
+  it('checks that if an element matches a given predicate "exists" returns true', () => {
+    const value: Value<String> = createValue("value");
+    expect(value.exists((s: String) => s == "value")).to.equal(true);
+  });
+
+  it('checks that if no element matches a given predicate "exists" returns false', () => {
+    const value: Value<String> = createValue("value");
+    expect(value.exists((s: String) => s == "other")).to.equal(false);
+  });
+
 });
 
 function createValue(value:String): Value<any> {
-    return {
-        get: () => value,
-        [Symbol.iterator]: () => null
+    let TestValue: {new(): Value<String>} = class extends Value<String> {
+        get(): String {
+            return value;
+        }
+        [Symbol.iterator](): Iterator<String> {
+            let consumed: number = 0;
+            return {
+                next(): IteratorResult<String> {
+                    return {
+                        done: !!consumed++,
+                        value
+                    };
+                }
+            };
+        }
     };
+    return new TestValue();
 }
